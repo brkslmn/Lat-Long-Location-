48bdc554dadb24511f0434e5d8f0a88976f341a8
Burak Salman <buraksalman84@gmail.com>
2022-10-31 20:20:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,33 @@ import pandas as pd
 app = Flask(__name__)
 geolocator = Nominatim(user_agent="geoapiExercises")
 app.config['JSON_AS_ASCII'] = False
-@app.route("/upload", methods=['GET', 'POST'])
+
+@app.route("/python/", methods=['GET', 'POST'])
+def upload_filee():
+    
+    return ''' <!doctype html>
+    <style>
+    a.button {
+    -webkit-appearance: button;
+    -moz-appearance: button;
+    appearance: button;
+
+    text-decoration: none;
+    color: initial;
+    }</style>
+    <title>Upload an excel file</title>
+    <a href="upload" class="button">Dosya Yükle</a>
+    <form action="post" method="get">
+      <label for="lat">First name:</label>
+      <input type="text" id="lat" name="lat"><br><br>
+      <label for="long">Last name:</label>
+      <input type="text" id="long" name="long"><br><br>
+      <input type="submit" value="Submit">
+    </form>
+    '''
+
+
+@app.route("/python/upload", methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # class adresses: 
@@ -30,10 +56,21 @@ def upload_file():
     <p><input type=file name=file><input type=submit value=Upload>
     </form>'''
 
-@app.route('/post/<string:lat>,<string:long>')
-def show_post(lat, long):
-    adress = str(geolocator.reverse(lat+","+long))
-    return f'{adress}'
+@app.route('/python/post')
+def show_post():
+    lat = str(request.args.get('lat'))
+    long = str(request.args.get('long'))
+    try:
+        adress = str(geolocator.reverse(lat+","+long))
+        response = app.response_class(
+            response=json.dumps(data),
+            status=200,
+            mimetype='application/json'
+        )
+        return  adress, 200
+    except:
+        return print("An exception occurred")
+    
 
 if __name__ == "__main__":
     app.run()
